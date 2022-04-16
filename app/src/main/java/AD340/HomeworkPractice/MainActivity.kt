e2ad4f3cbe7f2d4c56683547664ab01c9c05740e
Ashray Thapa <ashraythapa@gmail.com>
2022-04-15 23:49:55 -0700

    HW 2 Done (TextBox, Login and 'Toast' 2x2 Buttons)

--- app/src/main/java/AD340/HomeworkPractice/MainActivity.kt
+++ app/src/main/java/AD340/HomeworkPractice/MainActivity.kt
@@ -2,10 +2,33 @@ package AD340.HomeworkPractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clickLogin = findViewById<Button>(R.id.btn_login);
+        clickLogin.setOnClickListener {
+            Toast.makeText(this,"Login",Toast.LENGTH_SHORT).show()
+        }
+        val clickReg = findViewById<Button>(R.id.btn_register);
+        clickReg.setOnClickListener {
+            Toast.makeText(this,"Register",Toast.LENGTH_SHORT).show()
+        }
+        val clickSignUp = findViewById<Button>(R.id.btn_signup);
+        clickSignUp.setOnClickListener {
+            Toast.makeText(this,"Sign Up",Toast.LENGTH_SHORT).show()
+        }
+        val clickHello = findViewById<Button>(R.id.btn_hello);
+        clickHello.setOnClickListener {
+            Toast.makeText(this,"Hello",Toast.LENGTH_SHORT).show()
+        }
+        val clickWorld = findViewById<Button>(R.id.btn_world);
+        clickWorld.setOnClickListener {
+            Toast.makeText(this,"World",Toast.LENGTH_SHORT).show()
+        }
     }
 }
